cfe071dfedc1cf95f8b84a07523181af3860d156
Rafael Laurindo <rafaelfilholm@gmail.com>
2019-04-05 13:07:33 -0300

    feat: created get channel method in youtube services

--- src/services/YoutubeServices.ts
+++ src/services/YoutubeServices.ts
@@ -2,9 +2,14 @@ import axios from 'axios';
 
 class YoutubeServices {
 	private BASE_URL: string = 'https://www.youtube.com/';
+	private headers: any = {
+		'User-Agent':
+			'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
+		'Accept-Language': 'en'
+	};
 
-	getChannel(channelId: string): any {
-		return axios.get(`${this.BASE_URL}/channel/${channelId}/about`);
+	getChannel(channelId: string): Promise<any> {
+		return axios.get(`${this.BASE_URL}/channel/${channelId}/about`, { headers: this.headers });
 	}
 }
 
