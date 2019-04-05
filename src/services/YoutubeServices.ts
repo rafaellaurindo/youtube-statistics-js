a6a4bc9255e16cbe1c98769506093c4d1faa00b8
Rafael Laurindo <rafaelfilholm@gmail.com>
2019-04-05 13:18:29 -0300

    feat: created method in yt services to get base url

--- src/services/YoutubeServices.ts
+++ src/services/YoutubeServices.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 class YoutubeServices {
-	private BASE_URL: string = 'https://www.youtube.com/';
+	private static BASE_URL: string = 'https://www.youtube.com/';
+
 	private headers: any = {
 		'User-Agent':
 			'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
@@ -9,7 +10,11 @@ class YoutubeServices {
 	};
 
 	getChannel(channelId: string): Promise<any> {
-		return axios.get(`${this.BASE_URL}/channel/${channelId}/about`, { headers: this.headers });
+		return axios.get(`${YoutubeServices.getBaseUrl()}/channel/${channelId}/about`, { headers: this.headers });
+	}
+
+	static getBaseUrl(): string {
+		return YoutubeServices.BASE_URL;
 	}
 }
 
