68a28330ff29268bea914eec27cf61632dca3ecd
Rafael Laurindo <rafaelfilholm@gmail.com>
2019-04-05 13:11:46 -0300

    feat: created method to get channel details

--- src/YoutubeStatistics.ts
+++ src/YoutubeStatistics.ts
@@ -1,3 +1,20 @@
-class YoutubeStatistics {}
+import YoutubeServices from './services/YoutubeServices';
+import Channel from './models/Channel';
+
+class YoutubeStatistics {
+	private youtubeServices: YoutubeServices;
+
+	constructor() {
+		this.youtubeServices = new YoutubeServices();
+	}
+
+	async getChannel(channelId: string) {
+		const response = await this.youtubeServices.getChannel(channelId);
+		const channel = await new Channel();
+		const channelData = await channel.parse(response.data);
+
+		return channelData;
+	}
+}
 
 export default YoutubeStatistics;
--- src/models/Channel.ts
+++ src/models/Channel.ts
@@ -1,12 +1,9 @@
 import ChannelParser from '../parsers/ChannelParser';
 
 class Channel {
-	private data: any;
-
 	async parse(channelPageContent: string) {
 		const parser = await new ChannelParser();
-		const data = await parser.parse(channelPageContent);
-		this.data = await data;
+		return await parser.parse(channelPageContent);
 	}
 }
 
