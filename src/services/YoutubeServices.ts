5ccae91a7904bca5fd20e93b836f9ae4666c40da
Rafael Laurindo <rafaelfilholm@gmail.com>
2019-04-05 00:16:47 -0300

    feat: add getChannel method in Youtube services

--- src/services/YoutubeServices.ts
+++ src/services/YoutubeServices.ts
@@ -2,6 +2,10 @@ import axios from 'axios';
 
 class YoutubeServices {
 	private BASE_URL: string = 'https://www.youtube.com/';
+
+	getChannel(channelId: string): any {
+		return axios.get(`${this.BASE_URL}/channel/${channelId}/about`);
+	}
 }
 
 export default YoutubeServices;
